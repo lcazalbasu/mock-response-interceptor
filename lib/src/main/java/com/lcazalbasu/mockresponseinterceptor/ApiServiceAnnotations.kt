adf16d9edfbad0783985415b5a3a6ddb8737aae0
Laurentiu Cazalbasu <>
2024-10-28 22:31:37 +0200

    Update mock annotations and mock interceptor

--- lib/src/main/java/com/lcazalbasu/mockresponseinterceptor/ApiServiceAnnotations.kt
+++ lib/src/main/java/com/lcazalbasu/mockresponseinterceptor/ApiServiceAnnotations.kt
@@ -6,28 +6,85 @@ package com.lcazalbasu.mockresponseinterceptor
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-internal annotation class Mock(val enabled: Boolean)
+annotation class Mock(val enabled: Boolean)
 
 /**
- * Annotation to define a mock response for a retrofit API call.
+ * Annotation to define a mock response headers for a retrofit API call.
  * @param headers The headers of the response.
- * @param code The response code.
- * @param message The response message.
- * @param stringBody The response body as a json string.
- * @param fileBodyPath The path to the asset file containing the response body.
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-internal annotation class MockResponse(
-    val headers: Array<Header> = [],
-    val code: Int = 200,
-    val message: String = "",
-    val stringBody: String = "",
-    val fileBodyPath: String = "",
+annotation class MockResponseHeaders(
+    val headers: Array<Header>,
 ) {
 
+    /**
+     * Annotation to define a header for a mock response.
+     * @param name The name of the header.
+     * @param value The value of the header.
+     */
     annotation class Header(
         val name: String,
         val value: String,
     )
 }
+
+/**
+ * Annotation to define a mock response code for a retrofit API call.
+ * @param code The response code.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MockResponseCode(
+    val code: Int,
+)
+
+/**
+ * Annotation to define a mock response message for a retrofit API call.
+ * @param message The response message.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MockResponseMessage(
+    val message: String,
+)
+
+/**
+ * Annotation to define a json mock response body for a retrofit API call.
+ * @param jsonStringBody The response body as a json string.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MockResponseBody(
+    val jsonStringBody: String,
+)
+
+/**
+ * Annotation to define a file mock response body for a retrofit API call.
+ * @param fileBodyPath The path to the asset file containing the response body.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MockResponseFileBody(
+    val fileBodyPath: String,
+)
+
+/**
+ * Annotation to define a random mock response for a retrofit API call.
+ * @param randomStringBody The list of response bodies as json strings. One of these will be randomly selected.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RandomMockResponseBody(
+    val randomStringBody: Array<String>,
+)
+
+/**
+ * Annotation to define a random mock response for a retrofit API call.
+ * @param randomFileBodyPath The list of paths to the asset files containing the response bodies. One of these will be randomly selected.
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RandomMockResponseFileBody(
+    val randomFileBodyPath: Array<String>,
+)
--- lib/src/main/java/com/lcazalbasu/mockresponseinterceptor/MockResponseInterceptor.kt
+++ lib/src/main/java/com/lcazalbasu/mockresponseinterceptor/MockResponseInterceptor.kt
@@ -14,6 +14,33 @@ import retrofit2.Invocation
  * Interceptor that provides mock responses for Retrofit requests.
  *
  * @param context The context used to read the mock response from the assets folder.
+ *
+ * @see Mock
+ * @see MockResponseHeaders
+ * @see MockResponseCode
+ * @see MockResponseMessage
+ * @see MockResponseBody
+ * @see MockResponseFileBody
+ * @see RandomMockResponseBody
+ * @see RandomMockResponseFileBody
+ *
+ * If you are using the [MockResponseInterceptor] in a test environment,
+ * you can use the [Mock] annotation to enable or disable the mock response.
+ *
+ * If you will set the [Mock] annotation to true,
+ * you can use the [MockResponseHeaders] to set the response headers,
+ * [MockResponseCode] to set the response code,
+ * [MockResponseMessage] to set the response message.
+ *
+ * If you want to the response body you can use one of the following annotations:
+ * [MockResponseBody] to set the response body as a json string,
+ * [MockResponseFileBody] to set the response body from a file in the assets folder,
+ * [RandomMockResponseBody] to set a random response body from a list of strings,
+ * [RandomMockResponseFileBody] to set a random response body from a list of files in the assets folder.
+ * There annotations will be checked in the order they are listed.
+ *
+ * So, if you set both [MockResponseBody] and [MockResponseFileBody],
+ * the [MockResponseBody] will be used.
  */
 class MockResponseInterceptor(
     private val context: Context,
@@ -26,30 +53,35 @@ class MockResponseInterceptor(
         }
 
         val invocation = request.tag(Invocation::class.java)
-        val annotation = invocation?.method()?.getAnnotation(MockResponse::class.java)
             ?: return chain.proceed(request)
 
         val builder = Response.Builder()
-            .code(annotation.code)
-            .message(annotation.message)
-            .request(request)
+
+        // set the response code
+        val codeAnnotation = invocation.method().getAnnotation(MockResponseCode::class.java)
+        val code = codeAnnotation?.code ?: 200
+        builder.code(code)
+
+        // set the response message
+        val messageAnnotation = invocation.method().getAnnotation(MockResponseMessage::class.java)
+        val message = messageAnnotation?.message ?: "OK"
+        builder.message(message)
+
+        // set the request and protocol
+        builder.request(request)
             .protocol(chain.connection()?.protocol() ?: Protocol.HTTP_1_1)
 
-        for (header in annotation.headers) {
+        val headersAnnotation = invocation.method().getAnnotation(MockResponseHeaders::class.java)
+        val headers = headersAnnotation?.headers ?: emptyArray()
+
+        // set the response headers
+        for (header in headers) {
             builder.addHeader(header.name, header.value)
         }
 
-        val responseString = when {
-            annotation.stringBody.isNotBlank() ->
-                annotation.stringBody
-
-            annotation.fileBodyPath.isNotBlank() ->
-                getJsonDataFromAsset(annotation.fileBodyPath)
-
-            else -> ""
-        }
+        // set the response body
         builder.body(
-            responseString.toByteArray()
+            getResponseBody(invocation)
                 .toResponseBody("application/json".toMediaType()),
         )
 
@@ -61,6 +93,35 @@ class MockResponseInterceptor(
         val annotation = invocation?.method()?.getAnnotation(Mock::class.java)
             ?: return false
         return annotation.enabled
+    }
+
+    private fun getResponseBody(invocation: Invocation): String {
+        val stringBodyAnnotation = invocation.method().getAnnotation(MockResponseBody::class.java)
+        if (stringBodyAnnotation != null) {
+            return stringBodyAnnotation.jsonStringBody
+        }
+
+        val fileBodyAnnotation = invocation.method().getAnnotation(MockResponseFileBody::class.java)
+        if (fileBodyAnnotation != null) {
+            return getJsonDataFromAsset(fileBodyAnnotation.fileBodyPath)
+        }
+
+        val randomMockResponseAnnotation =
+            invocation.method().getAnnotation(RandomMockResponseBody::class.java)
+        if (randomMockResponseAnnotation != null) {
+            return randomMockResponseAnnotation.randomStringBody.randomOrNull() ?: ""
+        }
+
+        val randomMockResponseFileAnnotation =
+            invocation.method().getAnnotation(RandomMockResponseFileBody::class.java)
+        if (randomMockResponseFileAnnotation != null) {
+            val randomFile = randomMockResponseFileAnnotation.randomFileBodyPath.randomOrNull()
+                ?: return ""
+
+            return getJsonDataFromAsset(randomFile)
+        }
+
+        return ""
     }
 
     @Throws(IOException::class)
